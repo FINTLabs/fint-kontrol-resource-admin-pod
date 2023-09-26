027694775ff95065c426e190da15dda37bb3b39b
jennifer-fintlabs <jennifer@fintlabs.no>
2023-09-26 12:58:01 +0200

    added config to the proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,26 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/okonomi/faktura/fakturautsteder/',
-        createProxyMiddleware({
-            target: 'https://play-with-fint.felleskomponent.no',
+        createProxyMiddleware('/api/layout/configuration',{
+            // target: 'http://localhost:8080/beta/fintlabs-no',
+            target: 'http://localhost:8080',
             changeOrigin: true,
         })
     );
 
-    app.use(
-        '/api/member',
-        createProxyMiddleware({
-            target: 'http://localhost:8090',
-            changeOrigin: true,
-        })
-    );
-
-    app.use(
-        '/api/orgunits',
-        createProxyMiddleware({
-            target: 'http://localhost:8081',
-            changeOrigin: true,
-        })
-    );
 };
