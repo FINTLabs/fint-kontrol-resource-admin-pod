b68d8e572dfecfd3e7b60ba17760c2b8f3aa672d
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-11-01 10:36:55 +0100

    Types and repo changes

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,71 +1,116 @@
 // types.ts
+
 export interface IRole {
-    name: string,
-    accessRoleId: string
+	accessRoleId: string
+	name: string
+}
+
+// PermissionData is a role and its subsequent mapping to features and operation sets
+export interface IPermissionData {
+	accessRoleId: string
+	name: string
+	featureOperations: IFeatureOperation[]
+}
+
+export interface IFeatureOperation {
+	name: string
+	operations: string[]
+}
+
+// Features reside in a Role, but not as part of its type from the API
+export interface IFeature {
+	name: string
 }
 
 export interface IOrgUnit {
-    id: number;
-    name: string;
-    organisationUnitId: string;
-    parentRef: string;
-    parentName: null | string;
-    childrenRef: string[];
+	id: number
+	name: string
+	organisationUnitId: string
+	parentRef: string
+	parentName: null | string
+	childrenRef: string[]
 }
 
 export interface IOrgUnits {
-    totalItems: number;
-    orgUnits: IOrgUnit[];
-    totalPages: number;
-    currentPage: number;
+	totalItems: number
+	orgUnits: IOrgUnit[]
+	totalPages: number
+	currentPage: number
 }
 
 export interface IUserPage {
-    totalItems: number;
-    users: IUser[];
-    totalPages: number;
-    currentPage: number;
+	totalItems: number
+	users: IUser[]
+	totalPages: number
+	currentPage: number
 }
 
 export interface IUserListToBeReplaced {
-    users: IUser[]
+	users: IUser[]
 }
 
 export interface IUser {
-    id: number,
-    resourceId: string,
-    fullName: string,
-    userType: string,
-    userName: string,
+	id: number
+	resourceId: string
+	fullName: string
+	userType: string
+	userName: string
 }
 
 export type UsersContextState = {
-    basePath: string;
-    currentPage: number;
-    isAggregate: boolean,
-    isLoading: boolean,
-    itemsPerPage: number,
-    getUsersPage: () => void,
-    selected: number[],
-    setCurrentPage: (currentPageNumber: number) => void,
-    setIsLoading: (isLoading: boolean) => void,
-    setItemsPerPage: (paginationSize: number) => void,
-    setSelected: (selected: number[]) => void,
-    usersPage: IUserListToBeReplaced | null,
-};
+	basePath: string
+	currentPage: number
+	isAggregate: boolean
+	isLoading: boolean
+	itemsPerPage: number
+	getUsersPage: () => void
+	selected: number[]
+	setCurrentPage: (currentPageNumber: number) => void
+	setIsLoading: (isLoading: boolean) => void
+	setItemsPerPage: (paginationSize: number) => void
+	setSelected: (selected: number[]) => void
+	usersPage: IUserListToBeReplaced | null
+}
 
-export const contextDefaultValues: UsersContextState = {
-    basePath: "/",
-    currentPage: 1,
-    isAggregate: false,
-    isLoading: false,
-    itemsPerPage: 5,
-    setCurrentPage: (currentPageNumber: number) => void {},
-    getUsersPage(): void {},
-    setIsLoading(isLoading: boolean): void {},
-    selected: [],
-    setItemsPerPage: (paginationSize: number) => void {},
-    setSelected(selected: number[]): void {
-    },
-    usersPage: null,
-};
+export const userContextDefaultValues: UsersContextState = {
+	basePath: "/",
+	currentPage: 1,
+	isAggregate: false,
+	isLoading: false,
+	itemsPerPage: 5,
+	setCurrentPage: (currentPageNumber: number) => void {},
+	getUsersPage(): void {},
+	setIsLoading(isLoading: boolean): void {},
+	selected: [],
+	setItemsPerPage: (paginationSize: number) => void {},
+	setSelected(selected: number[]): void {},
+	usersPage: null
+}
+
+export type RoleContextState = {
+	basePath: string
+	currentPage: number
+	isAggregate: boolean
+	isLoading: boolean
+	itemsPerPage: number
+	permissionDataForRole: IPermissionData
+	selected: number[]
+	setCurrentPage: (currentPageNumber: number) => void
+	setIsLoading: (isLoading: boolean) => void
+	setItemsPerPage: (paginationSize: number) => void
+	setSelected: (selected: number[]) => void
+}
+
+export const roleContextDefaultValues: RoleContextState = {
+	basePath: "/",
+	currentPage: 1,
+	isAggregate: false,
+	isLoading: false,
+	itemsPerPage: 5,
+	permissionDataForRole: { accessRoleId: "", name: "", featureOperations: [{ name: "", operations: ["GET"] }] },
+	setCurrentPage: (currentPageNumber: number) => void {},
+	setIsLoading(isLoading: boolean): void {},
+	selected: [],
+	setItemsPerPage: (paginationSize: number) => void {},
+	setSelected(selected: number[]): void {}
+}
--- src/repositories/roles-repository.ts
+++ src/repositories/roles-repository.ts
@@ -1,15 +1,28 @@
-import axios from "axios";
-import {IRole} from "../api/types";
+import axios from "axios"
+import { IPermissionData, IRole } from "../api/types"
 
-const getAllRoles =
-    (basePath: string) => {
-        const baseUrl = `${basePath === '/' ? '' : basePath}/api/accessmanagement/v1/accessrole`;
-        const url = `${baseUrl}`;
-        return axios.get<IRole[]>(url);
-    }
+const getAllRoles = (basePath: string) => {
+	const baseUrl = `${basePath === "/" ? "" : basePath}/api/accessmanagement/v1/accessrole`
+	const url = `${baseUrl}`
+	return axios.get<IRole[]>(url)
+}
+
+const getFeaturesInRole = (basePath: string, roleId: string) => {
+	const baseUrl = `${basePath === "/" ? "" : basePath}/api/accessmanagement/v1/feature/${roleId}`
+	const url = `${baseUrl}`
+	return axios.get<IRole[]>(url)
+}
+
+const getPermissionDataForRole = (basePath: string, roleId: string) => {
+	const baseUrl = `${basePath === "/" ? "" : basePath}/api/accessmanagement/v1/permissionData/${roleId}`
+	const url = `${baseUrl}`
+	return axios.get<IPermissionData[]>(url)
+}
 
 const RolesRepository = {
-    getAllRoles
+	getAllRoles,
+	getFeaturesInRole,
+	getPermissionDataForRole
 }
 
 export default RolesRepository
