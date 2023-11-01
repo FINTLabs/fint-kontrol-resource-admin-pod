596bd9b9384e7e29728a5d3951b0e38faa7d5e60
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-11-01 10:36:30 +0100

    formatter configured

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,37 +1,58 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware")
 
-module.exports = function(app) {
-    app.use(
-        createProxyMiddleware('/api/orgunits', {
-            // Target port number must be equal to the port forwarded
-            target: 'http://localhost:8081/beta/fintlabs-no',
-            changeOrigin: true,
-            headers: {
-                Connection: 'keep-alive',
-            },
-        })
-    );
+module.exports = function (app) {
+	app.use(
+		createProxyMiddleware("/api/orgunits", {
+			// Target port number must be equal to the port forwarded
+			target: "http://localhost:8081/beta/fintlabs-no",
+			changeOrigin: true,
+			headers: {
+				Connection: "keep-alive"
+			}
+		})
+	)
 
-    app.use(
-        createProxyMiddleware('/api/accessmanagement/v1/user', {
-            // Target port number must be equal to the port forwarded
-            target: 'http://localhost:53989/beta/fintlabs-no',
-            changeOrigin: true,
-            headers: {
-                Connection: 'keep-alive',
-            },
-        })
-    );
+	app.use(
+		createProxyMiddleware("/api/accessmanagement/v1/user", {
+			// Target port number must be equal to the port forwarded
+			target: "http://localhost:53989/beta/fintlabs-no",
+			changeOrigin: true,
+			headers: {
+				Connection: "keep-alive"
+			}
+		})
+	)
 
-    app.use(
-        createProxyMiddleware('/api/accessmanagement/v1/accessrole', {
-            // Target port number must be equal to the port forwarded
-            target: 'http://localhost:53989/beta/fintlabs-no',
-            changeOrigin: true,
-            headers: {
-                Connection: 'keep-alive',
-            },
-        })
-    );
+	app.use(
+		createProxyMiddleware("/api/accessmanagement/v1/accessrole", {
+			// Target port number must be equal to the port forwarded
+			target: "http://localhost:53989/beta/fintlabs-no",
+			changeOrigin: true,
+			headers: {
+				Connection: "keep-alive"
+			}
+		})
+	)
 
-};
+	app.use(
+		createProxyMiddleware("/api/accessmanagement/v1/feature", {
+			// Target port number must be equal to the port forwarded
+			target: "http://localhost:53989/beta/fintlabs-no",
+			changeOrigin: true,
+			headers: {
+				Connection: "keep-alive"
+			}
+		})
+	)
+
+	app.use(
+		createProxyMiddleware("/api/accessmanagement/v1/accessrole/:id", {
+			// Target port number must be equal to the port forwarded
+			target: "http://localhost:53989/beta/fintlabs-no",
+			changeOrigin: true,
+			headers: {
+				Connection: "keep-alive"
+			}
+		})
+	)
+}
