645fb22276a398a4abb63d6e20ac70dbba4c95ec
“Christian-R-Haugland” <christian.haugland@itverket.no>
2023-11-21 10:43:37 +0100

    removed test query param

--- src/repositories/OrgUnitRepository.ts
+++ src/repositories/OrgUnitRepository.ts
@@ -2,7 +2,7 @@ import { IOrgUnitsPaginated } from "../api/types"
 import axios from "axios"
 
 const fetchUnitTreeData = (basePath: string) => {
-	const baseUrl = `${basePath === "/" ? "" : basePath}/api/orgunits&size=999`
+	const baseUrl = `${basePath === "/" ? "" : basePath}/api/orgunits`
 	return axios.get<IOrgUnitsPaginated>(baseUrl)
 }
 
